193685088e17251eb8fbd70481a3a65029f0b287
Aayus <godlord@AayushPathak>
2023-09-24 23:06:21 +0530

    ok

--- todo-app/app.js
+++ todo-app/app.js
@@ -5,17 +5,34 @@ const bodyParser = require("body-parser");
 const path = require("path");
 app.use(bodyParser.json());
 app.use(express.urlencoded({extended: false}));
+app.set("view engine", "ejs");
+const ejs = require('ejs');
+
+ejs.renderFile(path.join(__dirname, 'views', 'index.ejs'), { async: true }, (err, html) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(html);
+  }
+});
 
 // app.get("/", function (request, response) {
 //   response.send("Hello World");
 // });
 app.use(express.static(path.join(__dirname, "public")));
-app.set("view engine", "ejs");
 app.get("/", async (request, response) => { 
-  const allTodos = await Todo.getTodos();   
+  // const allTodos = await Todo.getTodos();   
   const overdue = await Todo.overdue();
+  const dueToday = await Todo.dueToday();
+  const dueLater = await Todo.dueLater();
+
   if(request.accepts("html")) {
-    response.render("index", { allTodos });
+    response.render("index", { 
+      title: "Todo List", 
+      overdue,
+      dueToday,
+      dueLater
+     });
   }
   else {
     response.json({
